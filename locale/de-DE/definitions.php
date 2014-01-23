a4089931bd85c566c4842ab60ecb919c4dd6a7a2
R-J <robin.jurinka@gmx.net>
2014-01-23 16:31:00 +0100

    Create definitions.php

---
+++ locale/de-DE/definitions.php
@@ -0,0 +1,5 @@
+<?php if (!defined('APPLICATION')) exit();
+$Definition['Your Flattr User Name'] = 'Dein Flattr Benutzername';
+$Definition['Flattr User'] = 'Flattr Benutzername';
+$Definition['FlattrThisImageAlt'] = 'Flattr this';
+$Definition['FlattrThisImageTitle'] = 'Flattr this';
